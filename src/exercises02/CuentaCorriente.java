2f34a9d9f39186426d0eec040326dd85541bd1d0
jaime98-coder <jaime.martin@iesnervion.es>
2026-02-08 22:13:52 +0100

    subida

---
+++ src/exercises02/CuentaCorriente.java
@@ -0,0 +1,17 @@
+package exercises02;
+
+public class CuentaCorriente {
+
+	String DNI;
+	String nombre;
+	Double saldo;
+	String nacionalidad;
+
+	public CuentaCorriente(String DNI, String nombre, Double saldo, String nacionalidad) {
+		super();
+		this.DNI = DNI;
+		this.nombre = nombre;
+		this.saldo = saldo;
+		this.nacionalidad = nacionalidad;
+	}
+}

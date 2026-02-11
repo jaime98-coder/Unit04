b9a9b168feb38bc92a327bc869cb5f9faa127a7b
jaime98-coder <jaime.martin@iesnervion.es>
2026-02-11 18:30:48 +0100

    subida

--- src/exercises02/CuentaCorriente.java
+++ src/exercises02/CuentaCorriente.java
@@ -4,14 +4,55 @@ public class CuentaCorriente {
 
 	String DNI;
 	String nombre;
-	Double saldo;
+	double saldo;
 	String nacionalidad;
 
-	public CuentaCorriente(String DNI, String nombre, Double saldo, String nacionalidad) {
+	public CuentaCorriente(String DNI, double saldo) {
+		super();
+		this.DNI = DNI;
+		this.saldo = saldo;
+
+	}
+
+	public CuentaCorriente(String DNI, String nombre, double saldo) {
+		super();
+		this.DNI = DNI;
+		this.nombre = nombre;
+		this.saldo = saldo;
+
+	}
+
+	public CuentaCorriente(String DNI, String nombre, double saldo, String nacionalidad) {
 		super();
 		this.DNI = DNI;
 		this.nombre = nombre;
 		this.saldo = saldo;
 		this.nacionalidad = nacionalidad;
+	}
+
+	public boolean sacarDinero(double saldo, double dinero) {
+		if (saldo >= dinero) {
+			saldo -= dinero;
+			System.out.println("Dinero sacado correctamente");
+			return true;
+		} else {
+			System.out.println("Saldo insuficiente");
+			return false;
+		}
+	}
+
+	public boolean ingresarDinero(double saldo, double dinero) {
+		if (dinero > 0) {
+			saldo += dinero;
+			System.out.println("Dinero ingresado correctamente");
+			return true;
+		} else {
+			System.out.println("No es posible introducir saldo iguales o superiores a 0");
+			return false;
+		}
+	}
+
+	public void mostrarDatos() {
+		
 	}
 }

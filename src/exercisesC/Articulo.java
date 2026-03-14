282987cb4824f0463aaaac6874bdf1d3b80355e5
jaime98-coder <jaime.martin@iesnervion.es>
2026-03-14 12:44:10 +0100

    Subida

--- src/exercisesC/Articulo.java
+++ src/exercisesC/Articulo.java
@@ -7,19 +7,15 @@ public class Articulo {
 	private int cuantosQuedan; // Stock
 
 	public Articulo(String nombre, double precio, int iva, int cuantosQuedan) {
-		if (!nombre.isBlank()) {
-			this.nombre = nombre;
-		}
-		if (precio > 0) {
-			this.precio = precio;
-		}
-		if (iva >= 0 && iva < 100) {
-			this.iva = iva;
-		}
+		this.setNombre(nombre);
+		this.setPrecio(precio);
+		this.setIva(iva);
+		this.setCuantosQuedan(cuantosQuedan);
 
-		if (cuantosQuedan >= 0) {
-			this.cuantosQuedan = cuantosQuedan;
-		}
+	}
+
+	public Articulo() {
+
 	}
 
 	public String getNombre() {
@@ -27,7 +23,12 @@ public class Articulo {
 	}
 
 	public void setNombre(String nombre) {
-		this.nombre = nombre;
+		if (!nombre.isBlank()) {
+			this.nombre = nombre;
+		} else {
+			System.out.println("Nombre erróneo introducido");
+			this.nombre = "Desconocido";
+		}
 	}
 
 	public double getPrecio() {
@@ -37,6 +38,9 @@ public class Articulo {
 	public void setPrecio(double precio) {
 		if (precio >= 0) {
 			this.precio = precio;
+		} else {
+			System.out.println("Precio erróneo");
+			this.precio = 0;
 		}
 	}
 
@@ -48,6 +52,9 @@ public class Articulo {
 
 		if (iva >= 0 && iva <= 100) {
 			this.iva = iva;
+		} else {
+			System.out.println("Impuesto erróneo");
+			this.iva = 0;
 		}
 	}
 
@@ -58,16 +65,67 @@ public class Articulo {
 	public void setCuantosQuedan(int cuantosQuedan) {
 		if (cuantosQuedan >= 0) {
 			this.cuantosQuedan = cuantosQuedan;
+		} else {
+			System.out.println("Cantidad errónea introducida");
+			this.cuantosQuedan = 0;
 		}
 	}
 
 	@Override
 	public String toString() {
-		return "Articulo [nombre=" + nombre + ", precio=" + precio + ", iva=" + iva + ", cuantosQuedan=" + cuantosQuedan
+		return "Articulo [nombre=" + nombre + ", precio sin =" + precio + ", iva=" + iva + ", cuantosQuedan=" + cuantosQuedan
 				+ "]";
 	}
 
+	// Añadido ejercicio C
+	public double getPVP() {
+		double precioConIVA;
+		precioConIVA = this.precio + (this.precio * this.iva / 100.0);
+		return precioConIVA;
+	}
 
+	/**
+	 * Método para aplicar descuento porcentual sobre el precio
+	 * 
+	 * @param descuento
+	 * @return precio con descuento
+	 */
+	public double getPVPDescuento(double descuento) {
+		double precioConDescuento;
+		double importeDescuento = 0;
+		// Validación para que se meta un descuento mayor a 0
+		if (descuento > 0) {
+			importeDescuento = (getPVP() * descuento / 100);
+		}
 
+		precioConDescuento = getPVP() - importeDescuento;
+		return precioConDescuento;
+	}
+
+	/**
+	 * Método para vender la cantidad de productos elegida
+	 * 
+	 * @param cantidad. Cantidad de productos a vender
+	 * @return TRUE --> Fue vendido correctamente. FALSE --> No fue posible,
+	 *         cantidad a vender es superior al stock actual
+	 */
+	public boolean vender(int cantidad) {
+		boolean fueVendido = false;
+		if (cantidad > 0 && this.cuantosQuedan >= cantidad) {
+			this.cuantosQuedan = this.cuantosQuedan - cantidad;
+			fueVendido = true;
+		}
+		return fueVendido;
+
+	}
+
+	public void almacenar(int cantidadAlmacenar) {
+		if (cantidadAlmacenar > 0) {
+			this.cuantosQuedan = this.cuantosQuedan + cantidadAlmacenar;
+		} else {
+			System.out.println("Cantidad a almacenar errónea");
+		}
+
+	}
 
 }
--- src/exercisesC/PrincipalArticulo.java
+++ src/exercisesC/PrincipalArticulo.java
@@ -1,13 +1,85 @@
 package exercisesC;
 
+import java.util.Scanner;
+
 public class PrincipalArticulo {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		Articulo articulo1 = new Articulo("Pijama", 10.0, 21, 50);
 		System.out.println(articulo1.toString());
+
 		Articulo articulo2 = new Articulo("Zapatillas", 4.0, 210, 34);
 		System.out.println(articulo2.toString());
 
+		Articulo articulo3 = new Articulo();
+		articulo3.setNombre(" ");
+		articulo3.setPrecio(7.3);
+		articulo3.setIva(10);
+		articulo3.setCuantosQuedan(73);
+		System.out.println(articulo3.toString());
+
+		Articulo articulo4 = new Articulo("Chaquetón", 72, 21, 42);
+		System.out.println(articulo4.toString());
+
+		// Ejercicio C para crear un método getPVP y asignarlo a una variable para
+		// sumarle el IVA al precio del producto
+		double precioConIVA;
+		precioConIVA = articulo1.getPVP();
+		System.out.println(precioConIVA);
+
+		// Uso del método getPVPDescuento
+		double precioConDescuento;
+		int descuento = -1;
+		while (descuento < 0 || descuento > 100) {
+			System.out.println("Introduce el % de descuento que deseas aplicarle al producto:");
+			descuento = sc.nextInt();
+
+			if (descuento < 0 || descuento > 100) {
+				System.out.println("Error: Introduce un descuento positivo");
+			}
+		}
+		precioConDescuento = articulo1.getPVPDescuento(descuento);
+		System.out.println("Precio del articulo 1 antes del descuento: " + precioConIVA);
+		System.out.println("Precio del articulo 1 con descuento aplicado: " + precioConDescuento);
+
+		// Uso del método vender
+		int cantidadVenta = 0;
+		while (cantidadVenta <= 0) {
+			System.out.println("Introduce el número de articulo que quieres vender");
+			cantidadVenta = sc.nextInt();
+			if (cantidadVenta <= 0) {
+				System.out.println("Error: Introduce un número de artículos positivo");
+			}
+		}
+		if (articulo1.vender(cantidadVenta)) {
+			System.out.println(cantidadVenta + " articulos vendidos correctamente ");
+			System.out.println("Stock restante: " + articulo1.getCuantosQuedan());
+		} else {
+			System.out.println("No es posible vender la cantidad de productos elegida, stock insuficiente");
+		}
+
+		// Uso del método almacenar
+		int cantidadAlmacenada = 0;
+		while (cantidadAlmacenada <= 0) {
+			System.out.println("Introduce el número de articulo que quieres almacenar");
+			cantidadAlmacenada = sc.nextInt();
+			if (cantidadAlmacenada <= 0) {
+				System.out.println("Error: Introduce un número de artículos positivo");
+			}
+		}
+		// Stock antes de almacenar
+		System.out.println("Stock antes de almacenar: " + articulo1.getCuantosQuedan());
+		// Almacenamos la cantidad introducida
+		articulo1.almacenar(cantidadAlmacenada);
+		// Stock después de almacenar
+		System.out.println("Stock después de almacenar: " + articulo1.getCuantosQuedan());
+
+		// Imprimimos el artículo para comprobar que se han actualizado los datos
+		// correctamente
+		System.out.println(articulo1.toString());
+
+		sc.close();
 	}
 
 }
